bb6ffc6c5bd6d605a58a1e989936811262d3887c
Thiago-Cez <thiago.silva.ts804@gmail.com>
2021-12-15 23:15:44 -0300

    orientacaoObjetos

---
+++ OrientacaoObjeto/src/br/com/OrientacaoObjeto/Exercicio1.java
@@ -0,0 +1,18 @@
+package br.com.OrientacaoObjeto;
+
+public class Exercicio1 {
+
+
+		String nome;
+		String sobrenome;
+		int idade;
+		
+		void comprar() {
+			System.out.println("Cliente comprando");
+		}
+		
+		void pesquisar() {
+			System.out.println("Cliente pesquisando preço");
+
+		  }
+}
---
+++ OrientacaoObjeto/src/br/com/OrientacaoObjeto/Exercicio2.java
@@ -0,0 +1,22 @@
+package br.com.OrientacaoObjeto;
+
+public class Exercicio2{
+
+	
+			String modelo;
+			int capacidade;
+			int distanciadochao;
+		
+			
+			void voando(int distancia) {
+				System.out.println("Avião voando");
+				
+				distanciadochao = distanciadochao + distancia;
+				
+			}
+			
+			void viajando() {
+				System.out.println("Avião está viajando");
+			}
+
+	}
